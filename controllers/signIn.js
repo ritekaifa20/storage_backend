0c2dfbcdeff925cdbf46a296f8b07e4d589f13b5
Ritek Mungali <ritek20122000@gmail.com>
2023-12-11 11:43:06 +0530

    cors validtaion

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,7 +1,7 @@
 const SignIn=require('../Modals/signinSchema')
 const jwt=require('jsonwebtoken')
 module.exports.SignInUser=async function(req,res){
-  
+  console.log("hello");
     try{
         let user = await SignIn.findOne({email: req.body.email});  
         if(!user) {
@@ -12,7 +12,7 @@ module.exports.SignInUser=async function(req,res){
                 return res.status(401).send({ accessToken: null, msg: "Invalid Password!" });
                 } else {
                     var token = jwt.sign({ id: user._id }, "himynameisritekmungali000nictomeetyou" , { expiresIn: '5h' });
-                    
+
                     return res.status(200).send({accessToken :token,msg:'successfully logged in',user:user});
                     }
                     }catch (err) {
